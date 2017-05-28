8a9a628a12f9af5c64f2899cdae8ef40c58ecfd2
Arifur Rahman <ar883@scarletmail.rutgers.edu>
2017-05-28 04:17:05 -0400

    test update/reminders

--- Python_NBA_Stat_Solo_project/tests/test_nba_py_shotchart.py
+++ Python_NBA_Stat_Solo_project/tests/test_nba_py_shotchart.py
@@ -2,5 +2,6 @@ from nba_py import shotchart
 from nba_py.player import get_player
 
 def test():
-    pid = get_player('Kevin', 'Durant')
-    assert shotchart.ShotChart(pid)
+    pid = get_player('Lebron', 'James') #find errror handler for invalid name
+    assert shotchart.ShotChart(pid)     #find any case-sensitive/player names including " - " , " ' "
+    
