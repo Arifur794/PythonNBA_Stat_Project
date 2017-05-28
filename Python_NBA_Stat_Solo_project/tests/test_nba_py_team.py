3a6f33fbbc700dd6840db9440fec10af5752edbb
Arifur Rahman <ar883@scarletmail.rutgers.edu>
2017-05-28 04:11:07 -0400

    breakdown visualized

--- Python_NBA_Stat_Solo_project/tests/test_nba_py_team.py
+++ Python_NBA_Stat_Solo_project/tests/test_nba_py_team.py
@@ -1,16 +1,26 @@
 from nba_py import team
+
+
 from nba_py.player import get_player
+
+
 from nba_py.constants import TEAMS
 
+
+
 def test():
+    
     team_id = TEAMS['ATL']['id']
+    
     player_id = get_player('Lebron', 'James')
     assert team.TeamList()
     assert team.TeamSummary(team_id)
+    
     team_details = team.TeamDetails(team_id)
     assert team_details
     assert team_details.background()
     assert team_details.history()
+    
     assert team.TeamCommonRoster(team_id)
     assert team.TeamGeneralSplits(team_id)
     assert team.TeamOpponentSplits(team_id)
@@ -27,4 +37,5 @@ def test():
     assert team.TeamShotTracking(team_id)
     assert team.TeamReboundTracking(team_id)
     assert team.TeamPassTracking(team_id)
+    
     assert team.TeamVsPlayer(team_id, player_id)
